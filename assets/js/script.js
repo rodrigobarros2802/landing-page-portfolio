4c485471ed6b4dca8bfd5fdc943847739b806ef2
Rodrigo Barros <rodrigobarros2802@gmail.com>
2022-10-11 21:28:24 -0300

    Editando lin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 function toggle() {
     var secao = document.getElementById('sec');
+    var nav = document.getElementById('nav');
     secao.classList.toggle('active');
+    nav.classList.toggle('active');
 }
